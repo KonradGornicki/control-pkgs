9be9b4ec8a118042c490fc98eebb1e2b9a425eeb
konrad <konje@github.com>
2019-09-06 12:35:35 +0100

    added my prbs.py function

--- arx_modelling/src/controller_disengage_prbs.py
+++ arx_modelling/src/controller_disengage_prbs.py
@@ -10,17 +10,27 @@ from std_msgs.msg import Float32
 
 from thrust_allocation.msg import pwm
 from sensor_msgs.msg import Joy
+
 x_flag = 0
+#prbs_sequence(N-number of steps,band-length of constant signal after transition, range-y values)
+prbs_seq = prbs_sequence(100,4,1) #initialize the prbs signal
+a = [] #intialize to empty list; it stores prbs signal that is sequentially shortened
+
 pub = rospy.Publisher('/mallard/cmd_vel',Twist, queue_size = 10)
 # pub_prbs = rospy.Publisher('mallard/prbs',)
 
 
-
 def callback(twist):
-    global x_flag 
+    global x_flag,a
+
     if (x_flag == 1):
         # value between 0 to 1 corresponds to 0-400 pwm 
-        twist.linear.x = 1
+        if (a): 
+            #pops first element and removes it from the list
+            twist.linear.x = a.pop(0)
+        else:
+            twist.linear.x = 0 #list is empty
+
     else:
         twist.linear.x = 0
     # unchanged:    
@@ -29,33 +39,27 @@ def callback(twist):
     pub.publish(twist)
 
 def callback_joy(data):
-    global x_flag
-    twist = Twist()
-    # twist.linear.x = 1*data.axes[1] 
-
+    global x_flag,a
     x_button = data.buttons[0] #on PS4 corresponds to "x" button 
-    #generate the prbs sequence:
     
-
     if (x_button == 1):
+        if (not a): #true only if a list is empty
+            a = prbs_seq[:] #set a list with entire pwm signal
         #set flag
         x_flag = 1
-        
     else:
+        if (a): #true if elements in the list
+            a = prbs_seq[:] #restore a list
         x_flag = 0
-    # pub.publish(twist)
 
 def velocity_allocation  ():
     rospy.init_node('controller_disengage') #, anonymous=True)
     rospy.Subscriber('/cmd_controller_konrad', Twist, callback)
     rospy.Subscriber('/joy',Joy,callback_joy)
-
     rospy.spin()
     # rospy.spin() simply keeps your node from exiting 
     # until the node has been shutdown.
 
 if __name__=='__main__':
     global prbs_seq
-    
-    prbs_seq = prbs_sequence(10,2,1)
     velocity_allocation()
