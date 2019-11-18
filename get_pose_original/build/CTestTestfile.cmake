bef9932612642019cc3915643396da157bbfa255
konrad <konje@github.com>
2019-11-18 11:28:07 +0000

    removed get pose origginal

--- get_pose_original/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/keir/catkin_ws/src/get_pose
-# Build directory: /home/keir/catkin_ws/src/get_pose/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
